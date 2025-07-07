708735dce35ce3162e4cc7499410f25855a853a0
Mawele <mbizi.kevin@gmail.com>
2025-07-07 15:30:17 +0200

    first commit

---
+++ scripts/Player/Racket.cs
@@ -0,0 +1,39 @@
+using Godot;
+using System;
+
+public partial class Racket : CharacterBody2D
+{
+    [Export] private int speed;
+    [Export] private int strength;
+    private Vector2 startPosition = new Vector2(1152/2, 576);
+
+    public override void _Ready()
+    {
+        Position = startPosition;
+    }
+
+    public override void _PhysicsProcess(double delta)
+    {
+        var velocity = Velocity;
+        Velocity = velocity;
+        GetInput();
+        MoveAndSlide();
+    }
+
+    private void GetInput()
+    {
+        var velocity = Velocity;
+        velocity.X = 0;
+
+        var right = Input.IsActionPressed("go_right");
+        var left = Input.IsActionPressed("go_left");
+
+        if (right)
+            velocity.X += speed;
+
+        if (left)
+            velocity.X -= speed;
+
+        Velocity = velocity;
+    }
+}
